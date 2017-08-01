8ed126c3d395cb4492cfcb64bfc50daef6668535
Andrei-Sergiu Pitis <andrei.pitis@lateral-inc.com>
2017-08-01 23:34:21 +0300

    Initial Commit

---
+++ FluidDynamicsMetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FluidDynamicsMetal
+//
+//  Created by Andrei-Sergiu Pițiș on 01/08/2017.
+//  Copyright © 2017 Andrei-Sergiu Pițiș. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

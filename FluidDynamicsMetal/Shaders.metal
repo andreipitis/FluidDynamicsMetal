68208d07e92955c9c4c388d266aacf2b4b63a5e2
Andrei-Sergiu Pitis <andrei.pitis@lateral-inc.com>
2017-08-15 01:30:13 +0300

    - added blit operation

--- FluidDynamicsMetal/Shaders.metal
+++ FluidDynamicsMetal/Shaders.metal
@@ -34,16 +34,16 @@ inline float SqDistPointSegment(float2 a, float2 b, float2 c)
 
 kernel void visualize(texture2d<float, access::sample> input [[texture(0)]], texture2d<float, access::write> output [[texture(1)]], uint2 gid [[thread_position_in_grid]], const device BufferData *bufferData [[buffer(0)]]) {
     float2 gidf = static_cast<float2>(gid);
-//    constexpr sampler fluid_sampler(coord::pixel, filter::nearest, address::clamp_to_edge);
-//
-//    float4 color = input.sample(fluid_sampler, gidf);
+
+    constexpr sampler fluid_sampler(coord::pixel, filter::nearest, address::clamp_to_edge);
+    float4 color = input.sample(fluid_sampler, gidf);
 
     if(bufferData->position.x != 0 && bufferData->position.y != 0) {
         float2 location = float2(bufferData->position.x, bufferData->position.y);
         float dist = sqrt(SqDistPointSegment(location, location, gidf));
         float2 pos = 1.0 * (1.0f - smoothstep(5.0f, 25.0f, dist));
-        output.write(float4(pos.x, pos.y, 0.0, 1.0), gid);
+        output.write(float4(pos.x, pos.y, 0.0, 1.0) + color, gid);
     } else {
-        output.write(float4(0.0, 0.0, 0.0, 1.0), gid);
+        output.write(float4(0.0, 0.0, 0.0, 1.0) + color, gid);
     }
 }
--- FluidDynamicsMetal/ViewController.swift
+++ FluidDynamicsMetal/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
     var initialTouchPosition: CGPoint?
     var touchDirection: CGPoint?
 
+    var inputTexture: MTLTexture!
+
+    private let scale = UIScreen.main.scale
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -42,6 +46,14 @@ class ViewController: UIViewController {
 
         let bufferSize = MemoryLayout<StaticData>.size
         staticBuffer = MetalDevice.sharedInstance.device.makeBuffer(length: bufferSize, options: .cpuCacheModeWriteCombined)
+
+        let textureDescriptor = MTLTextureDescriptor()
+        textureDescriptor.pixelFormat = .bgra8Unorm
+        textureDescriptor.usage = .unknown
+        textureDescriptor.width = Int(UIScreen.main.bounds.width * scale)
+        textureDescriptor.height = Int(UIScreen.main.bounds.height * scale)
+
+        inputTexture = MetalDevice.createTexture(descriptor: textureDescriptor)
     }
 
     override func didReceiveMemoryWarning() {
@@ -85,6 +97,7 @@ extension ViewController: MTKViewDelegate {
             let commandBuffer = MetalDevice.sharedInstance.newCommandBuffer()
 
             basicFilter.calculateWithCommandBuffer(buffer: commandBuffer, configureEncoder: { commandEncoder in
+                commandEncoder.setTexture(self.inputTexture, index: 0)
                 commandEncoder.setTexture(nextTexture, index: 1)
 
                 commandEncoder.setBuffer(self.staticBuffer, offset: 0, index: 0)
@@ -94,8 +107,20 @@ extension ViewController: MTKViewDelegate {
                 commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCouts)
             })
 
+//            commandBuffer.addCompletedHandler({ (commandBuffer) in
+//
+//            })
+
+            let blitEncoder = commandBuffer.makeBlitCommandEncoder()
+            let sourceSize = MTLSizeMake(Int(UIScreen.main.bounds.width * scale), Int(UIScreen.main.bounds.height * scale), 1)
+            let origin = MTLOriginMake(0, 0, 0)
+            blitEncoder.copy(from: nextTexture, sourceSlice: 0, sourceLevel: 0, sourceOrigin: origin, sourceSize: sourceSize, to: inputTexture, destinationSlice: 0, destinationLevel: 0, destinationOrigin: origin)
+            blitEncoder.endEncoding()
+
             commandBuffer.present(drawable)
             commandBuffer.commit()
+
+//            self.inputTexture = nextTexture
         }
 
         touchDirection = initialTouchPosition
